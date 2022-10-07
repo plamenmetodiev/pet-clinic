5ad3b83ef55f255bce1eca90e338d53e03df70d0
Plamen Metodiev <plamenm.metodiev@gmail.com>
2022-10-07 16:15:47 +0300

    Add Initial Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pet-clinit
+
 Web app pet clinic with pet, visitors, owners, and doctors.
---
+++ src/test/java/pm/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
